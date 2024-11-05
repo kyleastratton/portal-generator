7df45ea1f5bd7241a4a2cd0b40f15af2b594e13f
Kyle Stratton <kyle.stratton@icloud.com>
2024-11-05 01:07:38 +0000

    style changes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+function openNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 // ! Navbar logic
 
 function openTab(evt, tabName) {
